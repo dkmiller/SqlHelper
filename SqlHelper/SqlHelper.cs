62ee8e86d20ee3f34aa12524983f77fa26f0dee7
Daniel Miller <danmill@microsoft.com>
2018-03-12 14:56:54 -0700

    added SqlClient

---
+++ SqlHelper/SqlHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace SqlHelper
+{
+    public class SqlHelper
+    {
+        public static IEnumerable<T> Query<T>(string connectionString, string query, Func<SqlDataReader> load)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
