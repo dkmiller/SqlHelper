6e82298a911f703e5abc1dfaea83d7a9885494c0
Daniel Miller <danmill@microsoft.com>
2018-03-12 15:01:42 -0700

    added nuget data

--- SqlHelper/SqlHelper.cs
+++ SqlHelper/SqlHelper.cs
@@ -6,9 +6,22 @@ namespace SqlHelper
 {
     public class SqlHelper
     {
-        public static IEnumerable<T> Query<T>(string connectionString, string query, Func<SqlDataReader> load)
+        /// <summary>
+        /// Creates a connection to a SQL server using the given connection
+        /// string, executes the given query, and then returns the results
+        /// transformed via the given function.
+        /// </summary>
+        public static IEnumerable<T> Query<T>(string connectionString, string query, Func<SqlDataReader, T> load)
         {
-            throw new NotImplementedException();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var command = new SqlCommand(query, connection);
+
+                using (var reader = command.ExecuteReader())
+                    if (reader.HasRows)
+                        while (reader.Read())
+                            yield return load(reader);
+            }
         }
     }
 }
