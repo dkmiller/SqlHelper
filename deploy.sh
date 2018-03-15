bbff09fca572fca5d8c48d51afd7215a4e1d0c37
Daniel Miller <danmill@microsoft.com>
2018-03-15 08:53:17 -0700

    Pushing from /bin/Debug. If this works, dummy increment version before complete PR

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,4 @@
 ApiKey=$1
 Source=$2
 
-mono nuget.exe setApiKey $ApiKey -Source $Source -Verbosity quiet
-
-mono nuget.exe push ./SqlHelper/bin/Release/SqlWrapperLite.*.nupkg -Source $Source
+nuget push ./SqlHelper/bin/Debug/SqlWrapperLite.*.nupkg -Verbosity detailed -ApiKey $ApiKey -Source $Source
