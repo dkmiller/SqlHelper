86a7807cb7895308f5727e3b07d71c77269565f9
Daniel Miller <daniel.keegan.miller@gmail.com>
2018-08-10 18:21:40 -0700

    ready to ship test version (#9)

--- README.md
+++ README.md
@@ -2,4 +2,27 @@
 
 [![Build Status](https://travis-ci.org/dkmiller/SqlHelper.svg?branch=master)](https://travis-ci.org/dkmiller/SqlHelper)
 
-Lightweight wrapper around one-off SQL queries
+Lightweight wrapper around one-off SQL queries. This is _not_ heavily
+optimized, and should not be used for production scenarios which are grabbing
+massive amounts of dataor making hundreds of queries.
+
+On the other hand, if you just want to make a simple query and grab a few
+strings, you can just write code like the following.
+
+```csharp
+using SqlWrapperLite;
+
+var connStr = "<your connection string>";
+
+// Grab a few strings.
+IEnumerable<string> myStrings = SqlWrapper.Query<string>(connStr, "SELECT TOP 20 Name FROM RandomTable");
+
+// Grab some strongly typed pairs.
+IEnumerable<(string, int)> result = SqlWrapper.Query<string, int>(connStr, "SELECT Name, Age FROM OtherTable");
+
+// Extension method for SQL connections.
+using (var conn = new SqlConnection(connStr))
+{
+    var otherResult = conn.Query<(double?, int)>(connStr, "SELECT B, Age FROM OtherTable");
+}
+```
---
+++ SqlWrapperLite/SqlWrapperLite.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace SqlWrapperLite
+{
+    public static class SqlWrapper
+    {
+        /// <summary>
+        /// Creates a connection to a SQL server using the given connection
+        /// string, executes the given query, and then returns the results
+        /// transformed via the given function.
+        /// </summary>
+        public static IList<T> Query<T>(string connectionString, string query, Func<SqlDataReader, T> load)
+        {
+            IList<T> result;
+            using (var connection = new SqlConnection(connectionString))
+            {
+                result = Query<T>(connection, query, load);
+            }
+            return result;
+        }
+
+        public static IList<T> Query<T>(string connectionString, string query) =>
+            Query<T>(connectionString, query, r => (T)r.GetValue(0));
+
+        public static IList<(T1, T2)> Query<T1, T2>(string connectionString, string query) =>
+            Query<(T1, T2)>(connectionString, query, r => ((T1)r.GetValue(0), (T2)r.GetValue(1)));
+
+        /// <summary>
+        /// Executes the given query using the SQL connection, then returns the
+        /// results transformed via the given function. This does not dispose
+        /// the connection.
+        /// </summary>
+        public static IList<T> Query<T>(this SqlConnection connection, string query, Func<SqlDataReader, T> load)
+        {
+            var result = new List<T>();
+            using (var command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+                using (var reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            result.Add(load(reader));
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
+        public static IList<T> Query<T>(this SqlConnection connection, string query) =>
+            connection.Query<T>(query, r => (T)r.GetValue(0));
+
+        public static IList<(T1, T2)> Query<T1, T2>(this SqlConnection connection, string query) =>
+            connection.Query<(T1, T2)>(query, r => ((T1)r.GetValue(0), (T2)r.GetValue(1)));
+    }
+}
